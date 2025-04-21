dd786035de036916368460c39db52c288f1d6af0
Castanto69 <71986943+Castanto69@users.noreply.github.com>
2025-04-21 21:48:12 +0200

    Update gfapp.js

--- gfapp.js
+++ gfapp.js
@@ -26,8 +26,8 @@ function itsStillGoing() {
         return true;
     } else return false;
 }
-const nstart = new Date("04/01/2025" + " 00:00:00").getTime();
-const dstart = new Date("15/07/2025" + " 00:00:00").getTime();
+const nstart = new Date("01/04/2025" + " 00:00:00").getTime();
+const dstart = new Date("07/15/2025" + " 00:00:00").getTime();
 const mlenght = dstart - nstart;
 
 function COUNTDOWN() {
